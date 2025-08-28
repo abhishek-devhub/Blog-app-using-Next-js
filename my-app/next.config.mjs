01a2e3795961b3505b09ec7f7cd34a1f6dffc5eb
Abhishek Shukla <abhishekshukla8097@gmail.com>
2025-08-28 12:46:53 +0530

    added configs for some images

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+     images: {
+    domains: ['img.freepik.com'], // allow this domain
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)", "var(--font-roboto)", "sans-serif"],
+        serif: ["var(--font-merriweather)", "var(--font-lora)", "serif"],
+      },
+    },
+  },
+};
 
 export default nextConfig;
