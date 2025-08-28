3488ae06f76b043f224467f939f3be7c4ab4584a
Abhishek Shukla <abhishekshukla8097@gmail.com>
2025-08-28 17:43:51 +0530

    Schema for user and all authentication

--- my-app/components/PostingBlogs.js
+++ my-app/components/PostingBlogs.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import Image from 'next/image';
 import { useState } from 'react';
+import { useAuth } from '@/app/Context/AuthContext';
 
 const PostingBlogs = () => {
   const [Author, setAuthor] = useState('');
@@ -9,12 +10,32 @@ const PostingBlogs = () => {
   const [Content, setContent] = useState('');
   const [Category, setCategory] = useState('Technology');
 
-  function handlesubmit(e){
+  const isloggedIn = useAuth()
+
+  async function handlesubmit(e) {
     e.preventDefault();
-    const blogData = {Author, Title, Content, Category};
-    console.log(blogData);
+    const blogData = { Author, Title, Content, Category };
+    try {
+      const res = await fetch('/api/blog', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(blogData)
+      })
+      const data = await res.json()
+      console.log("Response: ", data);
+      if (res.ok) {
+        setAuthor('');
+        setTitle('');
+        setContent('');
+        setCategory('Technology');
+      }
+    } catch (err) {
+      console.log(err, "error in posting blog");
+    }
+
+    // console.log(blogData);
   }
-  
+
   return (
     <div>
       <h1 className='posts text-center text-violet-900 text-3xl font-bold m-2'>Posts your own Blogs Now Free</h1>
@@ -26,16 +47,16 @@ const PostingBlogs = () => {
           <form className='flex flex-col gap-3' onSubmit={handlesubmit}>
             {/* Author */}
             <label htmlFor="Name" className='author font-bold'>Author Name</label>
-            <input type="text" value={Author} className='bg-white rounded-lg p-2 border-2 border-black' onChange={(e)=>{setAuthor(e.target.value)}} />
+            <input type="text" value={Author} className='bg-white rounded-lg p-2 border-2 border-black' onChange={(e) => { setAuthor(e.target.value) }} />
             {/* Title */}
             <label htmlFor="Title" className='font-bold'>Blog Title</label>
-            <input type="text"  value={Title}  className='bg-white rounded-lg p-2 border-2 border-black' onChange={(e)=>{setTitle(e.target.value)}} />
+            <input type="text" value={Title} className='bg-white rounded-lg p-2 border-2 border-black' onChange={(e) => { setTitle(e.target.value) }} />
             {/* Content */}
             <label htmlFor="Content" className='font-bold' >Blog Content</label>
-            <textarea className='border-gray-400 border-2' name="Content" id="" cols="30" rows="10" value={Content} onChange={(e)=>{setContent(e.target.value)}}></textarea>
+            <textarea className='border-gray-400 border-2' name="Content" id="" cols="30" rows="10" value={Content} onChange={(e) => { setContent(e.target.value) }}></textarea>
             {/* Category */}
-            <label htmlFor="category"  className='font-bold'>Category : <span>
-              <select name="category" value={Category} onChange={(e)=>{setCategory(e.target.value)}} id="">
+            <label htmlFor="category" className='font-bold'>Category : <span>
+              <select name="category" value={Category} onChange={(e) => { setCategory(e.target.value) }} id="">
                 <option value="Technology">Technology</option>
                 <option value="Health">Health</option>
                 <option value="Travel">Travel</option>
@@ -49,7 +70,7 @@ const PostingBlogs = () => {
               </select>
             </span></label>
             {/* Submit Button */}
-            <button type='submit' className='postbtn flex p-2 text-2xl font-bold text-green-950 border-2 w-[130px] bg-violet-300 rounded-lg hover:backdrop-blur-lg cursor-pointer transition-transform duration-300 hover:scale-105'>Post Now</button>
+            {isloggedIn && <button type='submit' className='postbtn flex p-2 text-2xl font-bold text-green-950 border-2 w-[130px] bg-violet-300 rounded-lg hover:backdrop-blur-lg cursor-pointer transition-transform duration-300 hover:scale-105'>Post Now</button>}
           </form>
         </div>
       </div>
---
+++ my-app/models/BlogPost.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";        
+
+const BlogPostSchema = new mongoose.Schema({
+    "Author": {type: String, required: true},   
+    "Title": {type: String, required: true},
+    "Content": {type: String, required: true},
+    "Category": {type: String, required: true}
+})
+
+export default mongoose.models.BlogPost || mongoose.model('BlogPost', BlogPostSchema);
