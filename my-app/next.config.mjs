9429b5c08f7464ffed0daf7522bc691faafa6af3
Abhishek Shukla <abhishekshukla8097@gmail.com>
2025-08-28 21:46:07 +0530

    added some extra packages for Ui

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -3,14 +3,6 @@ const nextConfig = {
      images: {
     domains: ['img.freepik.com'], // allow this domain
   },
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["var(--font-inter)", "var(--font-roboto)", "sans-serif"],
-        serif: ["var(--font-merriweather)", "var(--font-lora)", "serif"],
-      },
-    },
-  },
 };
 
 export default nextConfig;
