c3570e750e87ca5f4bbe8274f28730da72172a95
Abhishek Shukla <abhishekshukla8097@gmail.com>
2025-09-10 22:44:52 +0530

    add comment section to blog posts

--- my-app/app/blog/[id]/page.js
+++ my-app/app/blog/[id]/page.js
@@ -7,6 +7,18 @@ const Blog = () => {
   
   const {id} = useParams();
   const [blog, setBlog] = useState('');
+  const [comment, setcomment] = useState('')
+  const [comments, setcomments] = useState([])
+
+  function CommentSection(comment){
+    console.log("Comment Posted: ", comment); 
+    setcomments([...comments, comment]);
+    setcomment('');
+    if(!comment){
+      alert("Comment cannot be empty");
+      return;
+    }
+  }
 
   useEffect(()=>{
     const fetchBlog = async()=>{
@@ -31,6 +43,18 @@ const Blog = () => {
       <p className="text-gray-600 mb-2">✍️ {blog.Author}</p>
       <p className="mb-4">{blog.Content}</p>
       <span className="px-3 py-1 bg-gray-200 rounded">{blog.Category}</span>
+      <div className="comments">
+        <h2 className="text-2xl font-semibold mt-6 mb-3">Comments</h2>
+        <div className="comment border-t pt-4">
+          <input type="text" placeholder='Add a comment' value={comment} className='comment p-2 m-2' onChange={(e)=> setcomment(e.target.value)}  />
+          <button onClick={()=> CommentSection(comment)} className="mt-2 px-4 py-2 bg-blue-500 text-white rounded">Post Comment</button>
+          {comments.map((cmt , index)=>(
+            <div key={index} className="border-b py-2">
+              <p>{cmt}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
