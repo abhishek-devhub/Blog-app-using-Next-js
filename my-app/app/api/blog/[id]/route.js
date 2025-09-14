51ff4ceda190595e3aeedfac7ef3c96af64f3948
Abhishek Shukla <abhishekshukla8097@gmail.com>
2025-09-14 08:58:33 +0530

    solved the error of the delete route

--- my-app/app/api/blog/[id]/route.js
+++ my-app/app/api/blog/[id]/route.js
@@ -8,3 +8,16 @@ export async function GET(req,{params}){
     const blog = await BlogPost.findById(id);
     return Response.json(blog);
 }
+
+export async function DELETE(req , {params}){
+    try{
+        await connectDB()
+        const {id} = params
+        const deleteblog = await BlogPost.findByIdAndDelete(id)
+        return Response.json(deleteblog)
+    }
+    catch(error){
+        console.log(error)
+    }
+    
+}
